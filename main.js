e79b0e2da26244204690470b1e4c60d79edc6e6a
bmeek <bmeek@semgroupcorp.com>
2020-03-27 14:50:59 -0500

    Add package.json and update manifest to remove unnecessary permissions

--- main.js
+++ main.js
@@ -5,59 +5,74 @@
 var api = 'https://baconipsum.com/api/';
 
 var apiOptions = {
-  type: 'meat-and-filler',
-  sentences: null,
-  paragraphs: 1,
-  startWithLorem: 1,
-  format: 'text',
-}
+	type: 'meat-and-filler',
+	sentences: null,
+	paragraphs: 1,
+	startWithLorem: 1,
+	format: 'text',
+};
 
-var regex = /^lorem:[ps][0-9]/g;
+var loremRegex = /^lorem:[psw][0-9]/g;
 
-document.addEventListener('input', function (evt) {
-  var el = evt.target.value;
-  if (el === undefined) return;
-  parseInput(evt);
+document.addEventListener('input', function(evt) {
+	var el = evt.target.value;
+	if (el === undefined) return;
+	parseInput(evt);
 });
 
 function parseInput(evt) {
-  var loremMatch = evt.target.value.match(regex);
-  if (loremMatch === null) return;
-  var sentenceOrParagraph = loremMatch[0].match(/[ps]/g)[0];
-  var howMany = loremMatch[0].match(/[0-9]/g)[0];
+	var loremMatch = evt.target.value.match(loremRegex);
+	if (loremMatch === null) return;
+	var options = loremMatch[0].match(/[psw]/g)[0];
+	var howMany = loremMatch[0].match(/[0-9]/g)[0];
 
-  var sentences = sentenceOrParagraph === 's' ? howMany : null;
-  var paragraphs = sentenceOrParagraph === 'p' ? howMany : null;
+	var sentences = options === 's' ? howMany : null;
+	var paragraphs = options === 'p' ? howMany : null;
+	var words = options === 'w' ? howMany : null;
 
-  var opts = {
-    ...apiOptions,
-    sentences,
-    paragraphs
-  };
+	if (words && +howMany > 20) paragraphs = howMany / 20;
 
-  callApi(buildApiUrl(api, opts), function (res) {
-    evt.target.value = res;
-  });
+	var opts = {
+		...apiOptions,
+		sentences,
+		paragraphs,
+		words,
+	};
+
+	callApi(buildApiUrl(api, opts), function(res) {
+		var val = res;
+		if (words)
+			val = res
+				.split(' ')
+				.filter((v, i) => i < howMany)
+				.join(' ');
+
+		console.log(opts);
+		evt.target.value = val;
+	});
 }
 
 function callApi(url, callback) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function () {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-      callback(xmlHttp.responseText);
-  }
-  xmlHttp.open("GET", url, true); // true for asynchronous 
-  xmlHttp.send(null);
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+			callback(xmlHttp.responseText);
+	};
+	xmlHttp.open('GET', url, true); // true for asynchronous
+	xmlHttp.send(null);
 }
 
 function buildApiUrl(url, opts) {
-  var type = `type=${opts.type}&`;
-  var sentences = opts.sentences == null ? '' : `sentences=${opts.sentences}&`;
-  var paragraphs = opts.paragraphs == null ? '' : `paragraphs=${opts.paragraphs}&`;
-  var startWithLorem = `startWithLorem=${opts.startWithLorem}&`;
-  var format = `format=${opts.format}`
+	var type = `type=${opts.type}&`;
+	var sentences =
+		opts.sentences == null ? '' : `sentences=${opts.sentences}&`;
+	var paragraphs =
+		opts.paragraphs == null ? '' : `paragraphs=${opts.paragraphs}&`;
 
-  if (sentences === null && paragraphs === null) sentences = 1
+	var startWithLorem = `startWithLorem=${opts.startWithLorem}&`;
+	var format = `format=${opts.format}`;
 
-  return `${url}?${type}${sentences}${paragraphs}${startWithLorem}${format}`;
+	if (sentences === null && paragraphs === null) sentences = 1;
+
+	return `${url}?${type}${sentences}${paragraphs}${startWithLorem}${format}`;
 }
